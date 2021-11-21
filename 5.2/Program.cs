1246dba826a6826cc9e7311eb06fcda916ce06ec
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-11-21 21:26:38 +0300

    Финализация

--- 5.2/Program.cs
+++ 5.2/Program.cs
@@ -10,6 +10,20 @@ namespace _5._2
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество элементов массива:\t");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int [N,N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i,j] = 1 - (i + j) % 2;
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
+
     }
 }
