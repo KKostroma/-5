ff93ca26bfe2e262f244e5c61133f2a34d9fec2b
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-11-21 19:12:27 +0300

    Финализация

--- Задание 5/Program.cs
+++ Задание 5/Program.cs
@@ -10,6 +10,19 @@ namespace Задание_5
     {
         static void Main(string[] args)
         {
+            int[] array = new int[7]; 
+            int S = 0;
+            int m = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                S += array[i];
+                m = S / 7;
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(m);
+            Console.ReadKey();
         }
     }
 }
